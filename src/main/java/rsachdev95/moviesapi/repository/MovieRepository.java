d2f724357e68cf6b5760e14f1032b0ca21a56e7f
Rohan <rsachdev95@gmail.com>
2019-05-18 16:03:26 +0100

    Add field annotation to models for mongo

--- src/main/java/rsachdev95/moviesapi/repository/MovieRepository.java
+++ src/main/java/rsachdev95/moviesapi/repository/MovieRepository.java
@@ -1,7 +1,7 @@
 package rsachdev95.moviesapi.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import rsachdev95.moviesapi.model.clientmodel.Movie;
+import rsachdev95.moviesapi.model.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
 }
