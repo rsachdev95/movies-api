60caf2170e405d8644b957f01d60f70c9fddc2cd
Rohan <rsachdev95@gmail.com>
2019-05-18 13:15:13 +0100

    WIP: add service

---
+++ src/main/java/rsachdev95/moviesapi/service/MovieService.java
@@ -0,0 +1,9 @@
+package rsachdev95.moviesapi.service;
+
+import rsachdev95.moviesapi.model.clientmodel.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+    List<Movie> findAll();
+}
